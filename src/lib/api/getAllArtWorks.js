615b9907dcb8023ef5d7a4bc460530fbd7f974f0
samiul-hasan-sysforge <samiulh085@gmail.com>
2026-06-25 22:52:45 +0600

    make getAllArtWorks fetch with cache is no-store

--- src/lib/api/getAllArtWorks.js
+++ src/lib/api/getAllArtWorks.js
@@ -1,6 +1,8 @@
 const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL
 
 export const getAllArtWorks = async () => {
-    const res = await fetch(`${baseUrl}/api/artwork`)
+    const res = await fetch(`${baseUrl}/api/artwork`, {
+        cache: "no-store"
+    })
     return await res.json()
 }
