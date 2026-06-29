01ef6df228c719113c8784d947455a8aa9608064
samiul-hasan-sysforge <samiulh085@gmail.com>
2026-06-29 07:41:23 +0600

    Final Fix

--- src/lib/api/getFeaturedArtworks.js
+++ src/lib/api/getFeaturedArtworks.js
@@ -2,7 +2,10 @@ const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL;
 
 export const getFeaturedArtworks = async () => {
     const res = await fetch(
-        `${baseUrl}/api/artwork?limit=6`
+        `${baseUrl}/api/artwork?limit=6`,
+        {
+            cache: "no-store"
+        }
     );
 
     const data = await res.json();
